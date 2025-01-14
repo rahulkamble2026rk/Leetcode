39c42f8f4fff974242cc848f21c865da69c25004
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-01-14 17:35:37 +0530

    Time: 35 ms (6.7%), Space: 91.4 MB (9.94%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,25 +1,53 @@
+// class Solution {
+// public:
+//     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B)  
+//     {
+//         vector<int>ans; 
+//         int n=A.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             ans.push_back(check(A,B,i));
+//         } 
+//         return ans;
+//     } 
+//     int check(vector<int>A,vector<int>B,int n)
+//     {  
+//         int count=0;
+//         for(int i=0;i<=n;i++)
+//         {
+//             if(find(B.begin(),B.begin()+n+1,A[i])!=B.begin()+n+1)
+//             {
+//                 count++;
+//             }
+//         } 
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B)  
-    {
-        vector<int>ans; 
-        int n=A.size();
+    { 
+        map<int,int>freqA; 
+        map<int,int>freqB;  
+        vector<int>ans;
+        int n=A.size();  
+        int commonCount=0;
         for(int i=0;i<n;i++)
         {
-            ans.push_back(check(A,B,i));
-        } 
-        return ans;
-    } 
-    int check(vector<int>A,vector<int>B,int n)
-    {  
-        int count=0;
-        for(int i=0;i<=n;i++)
-        {
-            if(find(B.begin(),B.begin()+n+1,A[i])!=B.begin()+n+1)
-            {
-                count++;
+            freqA[A[i]]++; 
+            freqB[B[i]]++; 
+            if (freqB[A[i]] == 1) 
+            { 
+                commonCount++;
+            } 
+             if(A[i] != B[i] && freqA[B[i]] == 1) 
+             { 
+                commonCount++;
             }
+             ans.push_back(commonCount);
         } 
-        return count;
+       return ans;
+        
     }
 };
