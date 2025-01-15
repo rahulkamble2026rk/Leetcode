ddd83d7ec8dd73ebd98d3b74c4de4c649791c9c6
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-01-15 19:12:43 +0530

    Time: 0 ms (100%), Space: 14 MB (18.59%) - LeetHub

---
+++ 2536-determine-if-two-events-have-conflict/2536-determine-if-two-events-have-conflict.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {     
+        int H1ofe1 = stoi(event1[0].substr(0, 2));
+        int M1ofe1 = stoi(event1[0].substr(3, 2));
+        int H2ofe1 = stoi(event1[1].substr(0, 2));
+        int M2ofe1 = stoi(event1[1].substr(3, 2));
+        
+        int H1ofe2 = stoi(event2[0].substr(0, 2));
+        int M1ofe2 = stoi(event2[0].substr(3, 2));
+        int H2ofe2 = stoi(event2[1].substr(0, 2));
+        int M2ofe2 = stoi(event2[1].substr(3, 2));
+
+        int start1 = H1ofe1 * 60 + M1ofe1;
+        int end1 = H2ofe1 * 60 + M2ofe1;
+        int start2 = H1ofe2 * 60 + M1ofe2;
+        int end2 = H2ofe2 * 60 + M2ofe2;
+
+        return !(end1 < start2 || end2 < start1);
+    }
+};
