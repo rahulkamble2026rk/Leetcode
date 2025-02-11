3d8e42a414bbe391d61ad1e8e61fbc73011893bf
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-02-11 19:27:23 +0530

    Time: 0 ms (100%), Space: 11.9 MB (54.86%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,77 @@
+// class Solution {
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+//     {   
+//         int m=obstacleGrid.size(); 
+//         int n=obstacleGrid[0].size();  
+//         vector<vector<int>>dp(m,vector<int>(n,-1));
+//         return count(m-1,n-1,obstacleGrid,dp);
+//     } 
+//     int count(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>&dp)
+//     {   
+//         if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
+//         {
+//             return 0;
+//         } 
+//         if(i<0 || j<0)
+//         {
+//             return 0;
+//         }  
+
+//         if(dp[i][j]!=-1)
+//         {
+//             return dp[i][j];
+//         }
+//         if(i==0 && j==0) 
+//         {
+//             return 1;
+//         } 
+        
+
+//         int up=count(i-1,j,obstacleGrid,dp); 
+//         int left=count(i,j-1,obstacleGrid,dp); 
+//         return dp[i][j]=up+left;
+//     }
+// }; 
+
+
+
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
     {   
         int m=obstacleGrid.size(); 
-        int n=obstacleGrid[0].size();  
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return count(m-1,n-1,obstacleGrid,dp);
-    } 
-    int count(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>&dp)
-    {   
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
+        int n=obstacleGrid[0].size();   
+        vector<vector<int>>dp(m,vector<int>(n,-1)); 
+        dp[0][0]=1;
+        for(int i=0;i<m;i++)
         {
-            return 0;
+            for(int j=0;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                } 
+                else if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                } 
+                else
+                {    
+                    int up=0;
+                    int left=0;
+                    if(i>=1)
+                    {
+                        up=dp[i-1][j];
+                    } 
+                    if(j>=1)
+                    {
+                        left=dp[i][j-1];
+                    } 
+                    dp[i][j]=up+left;
+                }
+            }
         } 
-        if(i<0 || j<0)
-        {
-            return 0;
-        }  
-          
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(i==0 && j==0) 
-        {
-            return 1;
-        } 
-        
-
-        int up=count(i-1,j,obstacleGrid,dp); 
-        int left=count(i,j-1,obstacleGrid,dp); 
-        return dp[i][j]=up+left;
+        return dp[m-1][n-1];
     }
 };
