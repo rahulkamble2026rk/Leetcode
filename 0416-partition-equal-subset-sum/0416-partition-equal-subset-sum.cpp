2f3c4815fd567d52a22d1624e7bd6a3868269939
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-02-14 20:47:13 +0530

    Time: 130 ms (48.72%), Space: 100.5 MB (29.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,49 +1,38 @@
 class Solution {
-public: 
+public:
     bool canPartition(vector<int>& nums) 
-    { 
-        int sumtotal = accumulate(nums.begin(), nums.end(), 0); 
-        if (sumtotal % 2) 
+    {
+        int sum=accumulate(nums.begin(),nums.end(),0);  
+        int n=nums.size();  
+        int target=sum/2; 
+        if(sum%2)
         {
             return false;
-        }
-        int target=sumtotal/2;
-        vector<vector<int>>dp(nums.size() + 1, vector<int>(target + 1, 0)); 
-        int n=nums.size();
-        for (int i = 0; i <= n; i++) 
-        {
-            dp[i][0] = true;  //0th col means capacity 0ke liye 
         }  
-        // Initialize 0th row to false (except dp[0][0])
-        for (int j = 1; j <= target; j++) {
-            dp[0][j] = false;
-        } 
-   
-     return dpapproach(nums,target,nums.size(),dp);
-        
-    }  
-
-    int dpapproach(vector<int>&nums,int target,int n,vector<vector<int>>dp)
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return partition(n-1,target,nums,dp);
+    } 
+    bool partition(int ind,int target,vector<int>&nums,vector<vector<int>>&dp)
     {
-        for(int i=1;i<=n;i++)
+        if(target==0) 
+        { 
+            return true;
+        } 
+        if(ind==0)
         {
-            for(int j=1;j<=target;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    int pi=dp[i-1][j-nums[i-1]]; 
-                    int pe=dp[i-1][j]; 
-                    dp[i][j] =pi||pe;
-                } 
-                else 
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
-            } 
-           
-         }
-          return dp[n][target];
-    }
+            return (nums[ind]==target);
+        } 
 
-    
+        if(dp[ind][target]!=-1)
+        {
+            return dp[ind][target];
+        }
+        bool nottake=partition(ind-1,target,nums,dp); 
+        bool take=false;
+        if(target>=nums[ind])
+        {
+            take=partition(ind-1,target-nums[ind],nums,dp);
+        } 
+        return dp[ind][target]=(take||nottake);
+    }
 };
