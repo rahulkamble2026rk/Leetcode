9d08e064217e709aa3a6d72f0a9085ca0ce5ec5a
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-02-18 13:10:30 +0530

    Time: 2221 ms (5.12%), Space: 598.1 MB (5.03%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) 
+    {   
+        int n=coins.size(); 
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int res=count(coins,amount,n-1,dp);
+        return (res>=1e9)?-1:res;
+    } 
+    int count(vector<int>coins,int amount,int ind,vector<vector<int>>&dp)
+    {
+        if(amount==0)
+        {
+            return 0;
+        }  
+        if(dp[ind][amount]!=-1)
+        {
+            return dp[ind][amount];
+        }
+        if(ind==0)
+        {
+            if(amount%coins[ind]==0)
+            {
+                return (amount/coins[ind]);
+            } 
+            else
+            {
+                return 1e9;
+            }
+        }
+
+        int nottake=0+count(coins,amount,ind-1,dp); 
+        int take=1e9; 
+        if(amount>=coins[ind])
+        {
+            take=1+count(coins,amount-coins[ind],ind,dp);  //as we are taking the element infinty so there is no need to take the ind-1 , beause we can take that element after word as well 
+        } 
+        return dp[ind][amount]=min(nottake,take);
+    }
+};
