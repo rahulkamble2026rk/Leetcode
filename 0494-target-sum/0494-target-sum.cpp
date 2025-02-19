b0052c0e3ff15bffa565ed2590361359180b69df
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-02-19 12:09:45 +0530

    Time: 2 ms (91.01%), Space: 15.4 MB (46.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) 
-    {  
-        int count=0;
-        findcount(nums,target,count,0,0); 
-        return count;
-    } 
-    void findcount(vector<int>&nums,int target,int& count,int index ,int sum)
-    {
-        if(index==nums.size())
-        {
-            if(sum==target)
-            {
-                count++;
-            } 
-            return;
-        } 
-        findcount(nums,target,count,index+1,sum+nums[index]); 
-         findcount(nums,target,count,index+1,sum-nums[index]); 
+    int findTargetSumWays(vector<int>& num, int target) {  
+        int n = num.size(); 
+        int sum = accumulate(num.begin(), num.end(), 0); 
+        
+        // Check if partitioning is possible
+        if ((sum - target) < 0 || (sum - target) % 2 != 0) {
+            return 0;
+        }
+        
+        int k = (sum - target) / 2;  // Corrected target calculation
+        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+        
+        return count(n - 1, k, num, dp);
+    }
+    
+    int count(int ind, int target, vector<int>& arr, vector<vector<int>>& dp) { 
+        if (ind == 0) {
+            if (target == 0 && arr[0] == 0) return 2;  // Two ways if first element is zero
+            if (target == 0 || target == arr[0]) return 1;
+            return 0;
+        }
+
+        if (dp[ind][target] != -1) return dp[ind][target];
+
+        int notTaken = count(ind - 1, target, arr, dp);
+        int taken = 0;
+        if (arr[ind] <= target) {
+            taken = count(ind - 1, target - arr[ind], arr, dp);
+        }
+        
+        return dp[ind][target] = notTaken + taken;
     }
 };
