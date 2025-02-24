4d66657849c2430d30d7e9abbf32ca1f7eb84ac2
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-02-24 19:03:00 +0530

    Time: 179 ms (73.68%), Space: 109.9 MB (94.74%) - LeetHub

---
+++ 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) 
+    {
+        priority_queue<int> pq; // Max-heap to store all top elements
+
+        for (int i = 0; i < grid.size(); i++)
+        {
+            int size = min(limits[i], (int)grid[i].size()); // Take min to avoid overflow
+            
+            // Sort only the required `limits[i]` elements in descending order
+            sort(grid[i].rbegin(), grid[i].rend()); 
+
+            for (int j = 0; j < size; j++) 
+            {   
+                pq.push(grid[i][j]); // Push top elements into max-heap
+            }
+        } 
+
+        long long sum = 0;
+        for (int i = 0; i < k && !pq.empty(); i++) 
+        {
+            sum += pq.top();
+            pq.pop();
+        } 
+        return sum;
+    }
+};
