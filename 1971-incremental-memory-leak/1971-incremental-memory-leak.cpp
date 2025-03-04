990e9a49b094f56d3dbe5469dcb9c0acd4cecccd
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-03-04 14:52:15 +0530

    Time: 3 ms (44.19%), Space: 8 MB (58.14%) - LeetHub

--- 1971-incremental-memory-leak/1971-incremental-memory-leak.cpp
+++ 1971-incremental-memory-leak/1971-incremental-memory-leak.cpp
@@ -3,32 +3,38 @@ public:
     vector<int> memLeak(int memory1, int memory2)  
     { 
          int second=1; 
-         while (true) 
-         {
-            if (memory1 >= memory2) {
-                if (memory1 >= second) {
-                    memory1 -= second;
-                } else {
-                    break; // memory1 cannot satisfy allocation
-                }
-            } else {
-                if (memory2 >= second) {
-                    memory2 -= second;
-                } else {
-                    break; // memory2 cannot satisfy allocation
-                }
-            }
-
-            // Check for potential overflow before incrementing
-            if (second == INT_MAX) 
+         
+        while(true)
+        {
+            if(memory1>=memory2)
             {
-                break; // Stop incrementing to avoid overflow
-            }
-             
+                if(memory1>=second)
+                {
+                    memory1-=second;
+                }   
+                else
+                {
+                    break;
+                }
+            } 
+            else
+            {
+               if(memory2>=second)
+                {
+                    memory2-=second;
+                }   
+                else
+                {
+                    break;
+                }
+            } 
+            
+            if(second==INT_MAX-1)
+            {
+                break;
+            } 
             second++;
-        }
-
-        return {second, memory1, memory2};
-    
+        } 
+        return {second,memory1,memory2};
     }
 };
