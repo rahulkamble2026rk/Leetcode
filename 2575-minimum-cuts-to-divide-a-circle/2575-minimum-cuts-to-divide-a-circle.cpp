7af33cfc8c2f3b98c0fd209c7edb60f373b8ca9f
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-03-05 15:34:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (48.22%) - LeetHub

---
+++ 2575-minimum-cuts-to-divide-a-circle/2575-minimum-cuts-to-divide-a-circle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfCuts(int n) 
+    {
+        if(n%2==0)
+        {
+            return n/2;
+        }  
+        if(n==1) return 0;
+        return n;
+    }
+};
