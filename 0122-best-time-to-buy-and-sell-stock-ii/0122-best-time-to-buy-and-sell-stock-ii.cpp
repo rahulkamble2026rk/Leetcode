c01a37da45f1d8d65bf47e57ebefee4298b01c05
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-04-08 16:31:40 +0530

    Time: 5 ms (20.61%), Space: 19.6 MB (26.36%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,34 +1,70 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) 
+//     {
+//         int buy=1;  
+//         vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+//         return maxprofit(prices,buy,0,dp);
+//     } 
+//     int maxprofit(vector<int>&prices,int buy,int ind,vector<vector<int>>&dp)
+//     {
+//         if(ind>=prices.size())  //when we purchase a share , but don't get chance to sell
+//         {
+//             return 0; 
+//         } 
+//         if(dp[ind][buy]!=-1)
+//         {
+//             return dp[ind][buy];
+//         } 
+//         int profit=0;
+//         if(buy)
+//         {
+//             int take=-prices[ind]+maxprofit(prices,0,ind+1,dp); 
+//             int nottake=0+maxprofit(prices,1,ind+1,dp); 
+//             profit=max(take,nottake);
+//         } 
+//         else 
+//         {
+//             int take=prices[ind]+maxprofit(prices,1,ind+1,dp); 
+//             int nottake=0+maxprofit(prices,0,ind+1,dp); 
+//             profit=max(take,nottake);
+//         } 
+//         return dp[ind][buy]=profit;
+//     }
+// }; 
+
+//By using the tabulzation(table , basse to top)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) 
     {
         int buy=1;  
-        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
-        return maxprofit(prices,buy,0,dp);
-    } 
-    int maxprofit(vector<int>&prices,int buy,int ind,vector<vector<int>>&dp)
-    {
-        if(ind>=prices.size())  //when we purchase a share , but don't get chance to sell
+        vector<vector<int>>dp(prices.size()+1,vector<int>(2,0)); 
+        
+        int n=prices.size();
+   
+        for(int ind=n-1;ind>=0;ind--)
         {
-            return 0; 
-        } 
-        if(dp[ind][buy]!=-1)
-        {
-            return dp[ind][buy];
-        } 
-        int profit=0;
-        if(buy)
-        {
-            int take=-prices[ind]+maxprofit(prices,0,ind+1,dp); 
-            int nottake=0+maxprofit(prices,1,ind+1,dp); 
-            profit=max(take,nottake);
-        } 
-        else 
-        {
-            int take=prices[ind]+maxprofit(prices,1,ind+1,dp); 
-            int nottake=0+maxprofit(prices,0,ind+1,dp); 
-            profit=max(take,nottake);
-        } 
-        return dp[ind][buy]=profit;
+            for(int j=0;j<2;j++)
+            {
+                int profit=0;
+                if(j==1)
+                {
+                    int take=-prices[ind]+dp[ind+1][0]; 
+                    int nottake=0+dp[ind+1][1]; 
+                    profit=max(take,nottake);
+                } 
+                else 
+                {
+                    int take=prices[ind]+dp[ind+1][1]; 
+                    int nottake=0+dp[ind+1][0];
+                    profit=max(take,nottake);
+                } 
+                dp[ind][j]=profit;
+            }
+        }  
+         return dp[0][1];  
     }
+       
+
 };
