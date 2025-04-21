6043bbf99b2bea2f896adb650436b4278efd8ffa
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-04-21 22:46:29 +0530

    Time: 3 ms (82.06%), Space: 11.6 MB (12.63%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string countAndSay(int n)  
+    {   
+        string ans2="";
+         ans(1,n,"1",ans2); 
+         return ans2;
+    } 
+    void ans(int i,int n,string str,string &str2)
+    {
+        if(i==n)
+        { 
+            str2=str;
+            return;
+        } 
+        map<char,int>stdmap; 
+        for(int i=0;i<str.size();i++)
+        {
+            stdmap[str[i]]++;
+        } 
+
+        string newstring=""; 
+        int count = 1;
+
+        for (int j = 1; j <= str.size(); j++) {
+            if (j < str.size() && str[j] == str[j - 1]) {
+                count++;
+            } else {
+                newstring += to_string(count);
+                newstring += str[j - 1];
+                count = 1;
+            }
+        }
+        ans(i+1,n,newstring,str2);
+    }
+};
