0047222e4eebe272b1021c9b3bb85c8488680cdc
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-04-22 17:57:24 +0530

    Time: 20 ms (5.26%), Space: 32.7 MB (5.08%) - LeetHub

--- 3773-minimum-pair-removal-to-sort-array-i/3773-minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/3773-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int minimumPairRemoval(vector<int>& nums) {
-        int steps = 0;
+    int minimumPairRemoval(vector<int>& nums) 
+    {
+        int steps=0;  
+        
+        while(!is_sorted(nums.begin(),nums.end()))
+            {     
 
-        // Continue until the array becomes sorted
-        while (!is_sorted(nums.begin(), nums.end())) {
-            int n = nums.size();
-            vector<pair<int, int>> record; // {sum, index}
-
-            // Rebuild record of all adjacent pair sums
-            for (int i = 0; i < n - 1; ++i) {
-                record.push_back({nums[i] + nums[i + 1], i});
-            }
-
-            // Sort to get the leftmost minimum-sum pair
-            sort(record.begin(), record.end());
-
-            int idx = record[0].second;
-
-            // Merge nums[idx] and nums[idx + 1] by replacing them with their sum
-            int sum = nums[idx] + nums[idx + 1];
-            nums[idx] = sum;
-            nums.erase(nums.begin() + idx + 1);
-
-            steps++;
-        }
-
+                vector<pair< int,pair<int,int>> >record; 
+                for(int i=0;i<nums.size()-1;i++)
+                {
+                    record.push_back({nums[i]+nums[i+1],{i,i+1}});
+                }  
+                sort(record.begin(),record.end()); 
+                int ind1=record[0].second.first; 
+                int ind2=record[0].second.second; 
+                
+                nums[ind1]=nums[ind1]+nums[ind2] ; 
+                nums.erase(nums.begin()+ind2); 
+                steps++;
+            } 
         return steps;
+        
     }
 };
