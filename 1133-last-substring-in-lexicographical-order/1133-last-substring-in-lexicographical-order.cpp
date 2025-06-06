47ba5530f87518e716a7972a4f338e82faf25902
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-06 19:31:16 +0530

    Time: 2080 ms (5.14%), Space: 23.1 MB (79.05%) - LeetHub

---
+++ 1133-last-substring-in-lexicographical-order/1133-last-substring-in-lexicographical-order.cpp
@@ -0,0 +1,38 @@
+// class Solution {
+// public:
+//     string lastSubstring(string s)  
+//     {
+//         //for the lexigrophically use the max(string,string) 
+         
+//          string res=""; 
+//          for(int i=0;i<s.size();i++)
+//          {  
+//             for(int j=0;j<s.size();j++)
+//             {
+//                 res=max(res,s.substr(i,j+1));
+//             }   
+//          } 
+//          return res;
+//     }
+// }; 
+
+#include <string_view>
+
+class Solution {
+public:
+    string lastSubstring(string s) {
+        char maxChar = *max_element(s.begin(), s.end());
+        std::string_view best = "";
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == maxChar) {
+                std::string_view candidate = std::string_view(s).substr(i);
+                if (candidate > best) {
+                    best = candidate;
+                }
+            }
+        }
+
+        return std::string(best);
+    }
+};
