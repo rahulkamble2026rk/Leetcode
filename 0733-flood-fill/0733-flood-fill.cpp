83702d1efc8fb6386f52717b06f31bfadd12b37f
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-12 14:48:33 +0530

    Time: 0 ms (100%), Space: 18.7 MB (11.29%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
-    {    
-        int n=image.size();
-        int m=image[0].size(); 
-        int intialcolour=image[sr][sc];
-        vector<vector<int>>visited=image; 
-        //  for(int i=0;i<n;i++)
-        //  {
-        //     for(int j=0;j<m;j++)
-        //     {
-        //         visited[i][j]=image[i][j];
-        //     }
-        //  }
-        queue<pair<pair<int,int>,int>>que;  
-        que.push({{sr,sc},color}); 
-          
-        visited[sr][sc]=color;
-        int delrow[]={0,1,0,-1}; 
-        int delcol[]={1,0,-1,0};
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)  
+    {   
+        int m=image.size(); 
+        int n=image[0].size();  
+          int originalColor = image[sr][sc];
+
+        // If the original color is already the target color, no change needed
+        if (originalColor == color) return image; 
+
+        vector<vector<int>>visited(m,vector<int>(n,0)); 
+
+        queue<pair<int,pair<int,int>>>que; 
+        que.push({originalColor,{sr,sc}}); 
+        visited[sr][sc]=1;  
+        vector<int>delrow={0,0,+1,-1}; 
+        vector<int>delcol={+1,-1,0,0};
+        bfs(image,que,visited,delrow,delcol,originalColor,color);  
+        image[sr][sc]=color;
+        return image;
+    } 
+    void bfs(vector<vector<int>>& image,queue<pair<int,pair<int,int>>>&que,vector<vector<int>>&visited, vector<int>delrow,vector<int>delcol, int originalColor,int newcolor) 
+    {  
+        int m=image.size(); 
+        int n=image[0].size();
         while(!que.empty())
         {
-            int currow=que.front().first.first; 
-            int curcol=que.front().first.second; 
-            int curcolor=que.front().second; 
-
+            pair<int,pair<int,int>>pt=que.front();  
             que.pop(); 
+            int x=pt.second.first; 
+            int y=pt.second.second; 
 
             for(int i=0;i<4;i++)
             {
-                int newrow=currow+delrow[i]; 
-                int newcol=curcol+delcol[i]; 
+                int new_x=x+delrow[i]; 
+                int new_y=y+delcol[i]; 
 
-                if(newrow>=0 && newrow<n && newcol>=0 && newcol<m && visited[newrow][newcol]!=curcolor && image[newrow][newcol]==intialcolour)
-                {
-                    que.push({{newrow,newcol},color}); 
-                    visited[newrow][newcol]=color;
+                if(new_x>=0 && new_x<m && new_y>=0 && new_y<n && visited[new_x][new_y]!=1 && image[new_x][new_y]==originalColor)
+                {   
+                    visited[new_x][new_y]=1;  
+                    image[new_x][new_y]=newcolor;
+                    que.push({originalColor,{new_x,new_y}});
                 }
             }
         }
-        
-        return visited;
     }
 };
