7af4097c84298a906e68710e6c29db5ba4adc160
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-12 19:39:00 +0530

    Time: 8 ms (24%), Space: 18.7 MB (38.3%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
-    {   
-        int n= numCourses; 
-        vector<int>indegree(n);
-        vector<vector<int>>adjList(n);
-         
-        //step1:create the adjList
-        for(int i=0;i<prerequisites.size();i++)
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+     vector<int>inDegree(numCourses,0);  
+        vector<vector<int>>adj_list(numCourses);
+        for(auto it:prerequisites)
         {
-            int x= prerequisites[i][0]; 
-            int y= prerequisites[i][1]; 
-            adjList[y].push_back(x); 
-            indegree[x]++;
+            int depend=it[0]; 
+            int preq=it[1];  
+            adj_list[preq].push_back(depend);  //Direct push karasthi vector chi size agoadar declare keli pahijech
         } 
-        
-        //step2:puhs the 0base indgree node in the queue 
-        queue<int>que; 
-        for(int i=0;i<indegree.size();i++)
+        for(int i=0;i<numCourses;i++)
         {
-            if(indegree[i]==0)
+            for(auto it:adj_list[i])
+            {
+                inDegree[it]++;
+            }
+        } 
+
+        queue<int>que; 
+        for(int i=0;i<numCourses;i++)
+        {
+            if(inDegree[i]==0)
             {
                 que.push(i);
             }
-        }
+        } 
 
-        //step3:traverse over the queue 
-        vector<int>ans; 
+        vector<int>topo; 
+
         while(!que.empty())
         {
-            int node=que.front();  
+            int node=que.front(); 
             que.pop(); 
-            ans.push_back(node); //this is the 0indgree element 
-            for(auto it:adjList[node])
+            topo.push_back(node); 
+            for(auto it:adj_list[node])
             {
-                indegree[it]--;  
-                if(indegree[it]==0)
+                inDegree[it]--;
+                if(inDegree[it]==0)
                 {
                     que.push(it);
                 }
-            } 
-
-        }
-        if(ans.empty())
+            }
+        } 
+        if(topo.size()!=numCourses)
         {
             return {};
         } 
-        return (ans.size() == numCourses) ? ans : vector<int>();
+        return topo;
     }
 };
