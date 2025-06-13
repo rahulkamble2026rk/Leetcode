2c361291bc218a828cca50d2f50330283a0f72b8
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-13 19:56:46 +0530

    Time: 98 ms (23.24%), Space: 49.3 MB (7.99%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,62 +1,60 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) 
+    int networkDelayTime(vector<vector<int>>& times, int n, int k)  
     {
-        //step1:  
-       vector<vector<pair<int, int>>> adjList(n + 1);
-
-        for(int i=0;i<times.size();i++)
+        //step1: formation of the adj_list 
+        vector<vector<pair<int,int>>>adj_list(n+1); 
+        for(auto it:times)
         {
-            int node=times[i][0]; 
-            int endnode=times[i][1]; 
-            int time=times[i][2]; 
-            
-            adjList[node].push_back({endnode,time}); 
+            adj_list[it[0]].push_back({it[1],it[2]}); 
         } 
 
-        //step2: 
-        vector<int>distance(n+1,INT_MAX); 
-        distance[k]=0; 
-         
-        //step3: 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>que; 
-         
-         if(adjList[k].empty()) 
-         {
-            return -1;
-         }
-        que.push({0,k}); 
-         
-        
-        while(!que.empty())
-        {
-            auto it=que.top(); 
-            int dist=it.first; 
-            int node=it.second; 
-            que.pop(); 
+ 
 
-            for(auto it2:adjList[node])
+        //step2:decalration of the distance , and parent vector 
+        vector<int>distance(n+1,1e9); 
+        vector<int>parent(n+1); 
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+        }
+        distance[k]=0;
+        dijkstra(adj_list,distance,parent,k);   
+        distance[0]=0; 
+        int maxtime=0;
+        for(int i=1;i<distance.size();i++)
+        {
+            if(distance[i]==1e9)
             {
-                int nodedist=it2.second; 
-                int endnode=it2.first; 
-                if(dist+nodedist<distance[endnode])
+                return -1;
+            } 
+            maxtime=max(maxtime,distance[i]);
+        }
+        return maxtime;
+    } 
+
+    void dijkstra(vector<vector<pair<int,int>>>adj_list,vector<int>&distance,vector<int>&parent,int src)
+    {
+        //there is use of the priority_queue means min-heap; 
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; 
+        pq.push({0,src}); 
+        while(!pq.empty())
+        {
+            pair<int,int>pt=pq.top(); 
+            pq.pop(); 
+            int dist=pt.first; 
+            int node=pt.second; 
+            for(auto it:adj_list[node])
+            {
+                int adjnode=it.first; 
+                int edge_weight=it.second; 
+                if(dist+edge_weight<distance[adjnode])
                 {
-                    distance[endnode]=dist+nodedist; 
-                    que.push({distance[endnode],endnode});
+                    distance[adjnode]=dist+edge_weight; 
+                    parent[adjnode]=node; 
+                    pq.push({distance[adjnode],adjnode});
                 }
             }
-        } 
-        
-        int maxTime = 0;
-        for (int i = 1; i <= n; i++) 
-        {
-            if (distance[i] == INT_MAX) 
-            {
-                return -1; 
-            }
-            maxTime = max(maxTime, distance[i]);
         }
-        
-        return maxTime;
     }
 };
