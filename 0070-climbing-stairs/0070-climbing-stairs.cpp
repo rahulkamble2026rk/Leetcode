7e4181d2d8ec32b35cd33ebaae02b5be7a3bf67f
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-16 20:35:55 +0530

    Time: 1 ms (6.54%), Space: 8.5 MB (35.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,31 +1,53 @@
+// class Solution {
+// public:
+//     int climbStairs(int n)  
+//     {   
+//         //distinct ways: total of the all occrances
+//         //can either one stair up or 2 and at the end their might be one 0   
+//         // 1.recursion fails due to tle
+//         //2.memoization: 
+//         //3.tabulazition:
+         
+//         vector<int>dp(n+1,-1);
+//         return countstairs(dp,n);
+//     } 
+//     int countstairs(vector<int>&dp,int n)
+//     { 
+//         if(n==0)
+//         {
+//             return 1;
+//         } 
+//         if(n==1)
+//         {
+//             return 1;
+//         }  
+//         if(dp[n]!=-1)
+//         {
+//             return dp[n];
+//         }
+//         int onestep=countstairs(dp,n-1); 
+//         int twostep=countstairs(dp,n-2); 
+//         return dp[n]=onestep+twostep;
+//     }
+// }; 
+
 class Solution {
 public:
     int climbStairs(int n)  
-    {   
-        //distinct ways: total of the all occrances
-        //can either one stair up or 2 and at the end their might be one 0   
-        // 1.recursion fails due to tle
-        //2.memoization:
-         
-        vector<int>dp(n+1,-1);
-        return countstairs(dp,n);
-    } 
-    int countstairs(vector<int>&dp,int n)
-    { 
-        if(n==0)
+    {     
+        if(n==0 || n==1)
         {
             return 1;
         } 
-        if(n==1)
+        vector<int>dp(n+1,0); 
+        dp[0]=1;  // this are the intial values
+        dp[1]=1; 
+
+        for(int i=2;i<=n;i++)
         {
-            return 1;
-        }  
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        int onestep=countstairs(dp,n-1); 
-        int twostep=countstairs(dp,n-2); 
-        return dp[n]=onestep+twostep;
+           dp[i]=dp[i-1]+dp[i-2];
+        } 
+        return dp[n];
     }
+    
 };
