8f293a23b2c7b64476ef24e0de01f979d48b4d06
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-16 20:17:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,58 +1,31 @@
-// class Solution {
-// public:
-//     int climbStairs(int n) 
-//     {
-//         //hete we have ask the number fo the ways then their will be use of the DP 
-//         //here we an perform either 1 step or 2 step climb so 
-//         return count(n);
-//     } 
-//     int count(int n)
-//     {
-//          if(n==0)
-//         {
-//             return 1;
-//         } 
-         
-//          if(n==1)
-//          {
-//             return 1;
-//          }
-//         int left=count(n-1);  //climbling the 1 step 
-//         int right=count(n-2);  //climbling the 2 step 
-//         return left+right;    //as we want the total ways
-//     }
-// }; 
-
 class Solution {
 public:
-    int climbStairs(int n) 
-    {
-        //memoization 
-        //step1: declate the vector 
-        vector<int>dp(n+1,-1); 
-        dp[0]=1; 
-        dp[1]=1;
-         count(n,dp); 
-         return dp[n];
+    int climbStairs(int n)  
+    {   
+        //distinct ways: total of the all occrances
+        //can either one stair up or 2 and at the end their might be one 0   
+        // 1.recursion fails due to tle
+        //2.memoization:
+         
+        vector<int>dp(n+1,-1);
+        return countstairs(dp,n);
     } 
-    int count(int n,vector<int>&dp)
-    {
-         if(n==0)
+    int countstairs(vector<int>&dp,int n)
+    { 
+        if(n==0)
         {
             return 1;
         } 
-         
-         if(n==1)
-         {
+        if(n==1)
+        {
             return 1;
-         } 
-
-         if(dp[n]!=-1)
-         {
+        }  
+        if(dp[n]!=-1)
+        {
             return dp[n];
-         }
-        int left=count(n-1,dp);  //climbling the 1 step 
-        int right=count(n-2,dp);  //climbling the 2 step 
-        return dp[n]=left+right;    //as we want the total ways step2 :storing the subproblems
+        }
+        int onestep=countstairs(dp,n-1); 
+        int twostep=countstairs(dp,n-2); 
+        return dp[n]=onestep+twostep;
     }
 };
