26bade8442c79363f2a7f4615b68f58c22f5f208
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-16 22:52:26 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,51 +1,30 @@
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) 
-//     {
-//         int n=nums.size();  
-//         vector<int>dp(n,-1);
-//         return maxrobb(n-1,nums,dp);
-//     } 
-//     int maxrobb(int ind,vector<int>&nums,vector<int>&dp)
-//     {   
-//          if(ind<0)
-//         {
-//             return 0;
-//         }
-//         if(ind==0)
-//         {
-//             return nums[ind];
-//         }  
-//         if(dp[ind]!=-1)
-//         {
-//             return dp[ind];
-//         } 
-       
-//         int pick=nums[ind]+maxrobb(ind-2,nums,dp); 
-//         int notpick=maxrobb(ind-1,nums,dp); 
-//         return dp[ind]=max(pick,notpick);
-//     }
-// }; 
-
-
 class Solution {
 public:
-    int rob(vector<int>& nums) 
-    {
-        int n=nums.size();  
-        vector<int>dp(n,-1); 
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++)
+    int rob(vector<int>& nums)  
+    {    
+        vector<int>dp(nums.size()+1,-1);
+        if(nums.size()==1)
         {
-            int pick=nums[i]; 
-            if(i>1)
-            {
-                pick+=dp[i-2];
-            } 
-            int notpick=0+dp[i-1]; 
-            dp[i]=max(pick,notpick);
-        }
-        return dp[n-1];
+            return nums[0];
+        } 
+       
+       int path1= maxAmountstolen(0,nums,dp); 
+       int path2=maxAmountstolen(1,nums,dp);
+        return max(path1,path2); 
     } 
-    
+    int maxAmountstolen(int ind ,vector<int>&nums,vector<int>&dp)
+    {       
+            if(ind>=nums.size())
+            {
+                return 0;
+            } 
+            if(dp[ind]!=-1)
+            {
+                return dp[ind];
+            }
+             
+            int take=nums[ind]+ maxAmountstolen(ind+2,nums,dp); 
+            int skip= maxAmountstolen(ind+1,nums,dp); 
+            return dp[ind]=max(take,skip);
+    }
 };
