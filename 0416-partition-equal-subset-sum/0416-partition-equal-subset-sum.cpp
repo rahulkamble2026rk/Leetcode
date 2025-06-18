c4ef6e0559f8a858cbd68bb2d0c1d5f469a68fee
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-18 20:02:18 +0530

    Time: 166 ms (24.8%), Space: 110.4 MB (38.78%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) 
-    {
-        int sum=accumulate(nums.begin(),nums.end(),0);  
-        int n=nums.size();  
-        int target=sum/2; 
-        if(sum%2)
-        {
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0); 
+        if (sum % 2 != 0) {
             return false;
-        }  
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return partition(n-1,target,nums,dp);
-    } 
-    bool partition(int ind,int target,vector<int>&nums,vector<vector<int>>&dp)
-    {
-        if(target==0) 
-        { 
-            return true;
-        } 
-        if(ind==0)
-        {
-            return (nums[ind]==target);
-        } 
+        }   
 
-        if(dp[ind][target]!=-1)
-        {
+        int target = sum / 2;
+        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
+        return check(nums, 0, target, dp);
+    } 
+
+    bool check(vector<int>& nums, int ind, int target, vector<vector<int>>& dp) {    
+        if (target == 0) return true;
+        if (ind >= nums.size()) return false;
+
+        if (dp[ind][target] != -1)
             return dp[ind][target];
-        }
-        bool nottake=partition(ind-1,target,nums,dp); 
-        bool take=false;
-        if(target>=nums[ind])
-        {
-            take=partition(ind-1,target-nums[ind],nums,dp);
-        } 
-        return dp[ind][target]=(take||nottake);
+
+        bool nottake = check(nums, ind + 1, target, dp); 
+        bool take = false; 
+        if (target >= nums[ind])
+            take = check(nums, ind + 1, target - nums[ind], dp);
+
+        return dp[ind][target] = (take || nottake);
     }
 };
-
-//tabulazation appraoch :
