de1410720b017142cb06d5cd2c415119b966f2fe
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-18 20:02:41 +0530

    Time: 159 ms (28.17%), Space: 110.5 MB (31.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0); 
-        if (sum % 2 != 0) {
+    bool canPartition(vector<int>& nums) 
+    {
+        //we cannot divide the if the sum is odd 
+        int sum=accumulate(nums.begin(),nums.end(),0); 
+        if(sum%2!=0)
+        {
             return false;
         }   
-
-        int target = sum / 2;
-        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
-        return check(nums, 0, target, dp);
+        int target=sum/2;
+        vector<vector<int>>dp(nums.size(),vector<int>(target+1,-1));
+        return check(nums,0,sum/2,dp);
     } 
 
-    bool check(vector<int>& nums, int ind, int target, vector<vector<int>>& dp) {    
-        if (target == 0) return true;
-        if (ind >= nums.size()) return false;
-
-        if (dp[ind][target] != -1)
+    bool check(vector<int>&nums,int ind,int target,vector<vector<int>>&dp)
+    {    
+        if(target==0)
+        {
+            return 1;
+        }  
+         if(ind>=nums.size())
+        {
+            return 0;
+        } 
+        if(dp[ind][target]!=-1)
+        {
             return dp[ind][target];
-
-        bool nottake = check(nums, ind + 1, target, dp); 
-        bool take = false; 
-        if (target >= nums[ind])
-            take = check(nums, ind + 1, target - nums[ind], dp);
-
-        return dp[ind][target] = (take || nottake);
+        }
+        
+       
+        int nottake=check(nums,ind+1,target,dp); 
+        int take=0; 
+        if(target>=nums[ind])
+        {
+            take=check(nums,ind+1,target-nums[ind],dp);
+        } 
+        return dp[ind][target]=(take||nottake);
     }
 };
