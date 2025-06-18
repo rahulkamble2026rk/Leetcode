4ab96a379fcb67063a4224a327ae6b5ad3a88549
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-18 20:27:47 +0530

    Time: 207 ms (11.29%), Space: 17.5 MB (57.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,3 +1,45 @@
+// class Solution {
+// public:
+//     bool canPartition(vector<int>& nums) 
+//     {
+//         //we cannot divide the if the sum is odd 
+//         int sum=accumulate(nums.begin(),nums.end(),0); 
+//         if(sum%2!=0)
+//         {
+//             return false;
+//         }   
+//         int target=sum/2;
+//         vector<vector<int>>dp(nums.size(),vector<int>(target+1,-1));
+//         return check(nums,0,sum/2,dp);
+//     } 
+
+//     bool check(vector<int>&nums,int ind,int target,vector<vector<int>>&dp)
+//     {    
+//         if(target==0)
+//         {
+//             return 1;
+//         }  
+//          if(ind>=nums.size())
+//         {
+//             return 0;
+//         } 
+//         if(dp[ind][target]!=-1)
+//         {
+//             return dp[ind][target];
+//         }
+        
+       
+//         int nottake=check(nums,ind+1,target,dp); 
+//         int take=0; 
+//         if(target>=nums[ind])
+//         {
+//             take=check(nums,ind+1,target-nums[ind],dp);
+//         } 
+//         return dp[ind][target]=(take||nottake);
+//     }
+// }; 
+
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) 
@@ -7,34 +49,31 @@ public:
         if(sum%2!=0)
         {
             return false;
-        }   
-        int target=sum/2;
-        vector<vector<int>>dp(nums.size(),vector<int>(target+1,-1));
-        return check(nums,0,sum/2,dp);
+        }    
+
+   
+        int n = nums.size();
+        int target = sum / 2;
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        
+        // Base case: empty subset can achieve sum 0
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        } 
+
+         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                // Don't take the current element
+                dp[i][j] = dp[i - 1][j];
+                
+                // Take the current element if possible
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        } 
+          return dp[n][target];
     } 
 
-    bool check(vector<int>&nums,int ind,int target,vector<vector<int>>&dp)
-    {    
-        if(target==0)
-        {
-            return 1;
-        }  
-         if(ind>=nums.size())
-        {
-            return 0;
-        } 
-        if(dp[ind][target]!=-1)
-        {
-            return dp[ind][target];
-        }
-        
-       
-        int nottake=check(nums,ind+1,target,dp); 
-        int take=0; 
-        if(target>=nums[ind])
-        {
-            take=check(nums,ind+1,target-nums[ind],dp);
-        } 
-        return dp[ind][target]=(take||nottake);
-    }
+   
 };
