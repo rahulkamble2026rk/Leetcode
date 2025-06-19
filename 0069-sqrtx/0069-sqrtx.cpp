b4ccfd8a0b4e96530923ef8c7b91ff3e621f62f3
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-19 19:14:26 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     int mySqrt(int x)  
     {
-        return pow(x,0.5);
+        long long  ans=9999; 
+        long long  low=0; 
+        long long high=x; 
+        while(low<=high)
+        {
+            long long mid=(low+high)/2; 
+            if(mid*mid<=x)
+            {
+                ans=mid; 
+                low=mid+1;
+            } 
+            else
+            {
+                high=mid-1;
+            }
+        } 
+        return ans;
     }
 };
