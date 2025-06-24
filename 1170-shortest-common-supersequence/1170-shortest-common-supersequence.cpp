518e07f7fb89517c41eab7172d14154711ee0612
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-24 23:06:24 +0530

    Time: 27 ms (39.36%), Space: 26.8 MB (43.6%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,98 +1,50 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) 
+    string shortestCommonSupersequence(string s, string p)  
     {
-        //Firstly find teh LCS by tabulzation:  
-        int size1=str1.size(); 
-        int size2=str2.size();
-        vector<vector<int>>dp(size1+1,vector<int>(size2+1,0)); 
+        int size1 = s.size(); 
+        int size2 = p.size();
 
-        for(int i=0;i<=size1;i++)
-        {
-            if(i==0)
-            {
-                dp[i][0]=0;
-            }
-        }   
+        vector<vector<int>> dp(size1+1, vector<int>(size2+1, 0)); 
 
-        for(int j=0;j<=size2;j++)
-            {  
-                if(j==0)
-                {
-                    dp[0][j]=0;
-                }
-            } 
-
-
-
-        for(int i=1;i<=size1;i++)
-        {
-            for(int j=1;j<=size2;j++)
-            {
-                if(str1[i-1]==str2[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                } 
-                else
-                {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        for (int i = 1; i <= size1; i++) {
+            for (int j = 1; j <= size2; j++) {
+                if (s[i-1] == p[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-        }  
- 
+        }
 
-        // for(int i=0;i<=size1;i++)
-        // {
-        //     for(int j=0;j<=size2;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     } 
-        //     cout<<endl;
-        // }
-        //step2: print the lcs string
-        string ans=""; 
-        int i=size1,j=size2;  
-        
-        while(i>0 && j>0)
-        {
-            if(str1[i-1]==str2[j-1])
-            {
-                ans+=str1[i-1]; 
-                // cout<<"I am going "<<str1[i-1]<<endl;
-                i--; 
-                j--;
-            } 
-            else if(dp[i-1][j]>dp[i][j-1])
-            {
-                ans+=str1[i-1];  
-                // cout<<"I am going "<<str1[i-1]<<endl;
+        // Reconstruct SCS from DP table
+        string result = "";
+        int i = size1, j = size2;
+        while (i > 0 && j > 0) {
+            if (s[i-1] == p[j-1]) {
+                result += s[i-1];
+                i--; j--;
+            } else if (dp[i-1][j] > dp[i][j-1]) {
+                result += s[i-1];
                 i--;
-            } 
-            else 
-            {
-                ans+=str2[j-1];  
-                // cout<<"I am going "<<str2[j-1]<<endl;
+            } else {
+                result += p[j-1];
                 j--;
             }
-        } 
- 
+        }
 
- //step3:this step is for the last remaingin element which remains at ind=0
-        while(i>0)
-        {
-            ans+=str1[i-1]; 
+        // Add remaining characters
+        while (i > 0) {
+            result += s[i-1];
             i--;
-        }  
-        while(j>0)
-        {
-            ans+=str2[j-1]; 
+        }
+        while (j > 0) {
+            result += p[j-1];
             j--;
         }
 
-
-        //as we traverse from the bottom of the table to the top of the table so there is the reverse string is get printed  
-        //so reverse the string  
-        reverse(ans.begin(),ans.end()); 
-        return ans;
-    }
+        // Since we built it backwards
+        reverse(result.begin(), result.end());
+        return result;
+    }  
 };
