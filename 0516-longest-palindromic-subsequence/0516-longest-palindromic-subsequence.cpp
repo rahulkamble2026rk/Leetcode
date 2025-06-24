6dbfd8fa7c16d161e695594c64c0c61c80ac21f2
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-24 19:06:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) 
+    int longestPalindromeSubseq(string s)  
     {
-        
-       
-        int ind1 = s.size(); 
-        int ind2 = s.size(); 
-        string text1=s; 
-        reverse(s.begin(),s.end()); 
-        string text2=s;
-        vector<vector<int>> dp(ind1 + 1, vector<int>(ind2 + 1, 0));
+        //longest pallindromic subsequene is nothing but that revese teh current string , and use that string as the another string, and then find the lcs 
 
-        for(int i=0;i<ind1;i++)
+        int size1=s.size(); 
+        string p=s; 
+        reverse(p.begin(),p.end()); 
+        return countLongestPallindromic(size1,size1,s,p)-1;   
+    }  
+    int countLongestPallindromic(int ind1,int ind2,string first,string second)
+    {
+        if(ind1<0 || ind2<0)
         {
-            dp[i][0]=0;     //if the one of the index is 0 then it will give 0
+            return 0;
         } 
-        for(int j=0;j<ind2;j++)
+
+        //If mathcing: 
+        if(first[ind1]==second[ind2])
         {
-            dp[0][j]=0;
-        }
-        for (int i = 1; i <= ind1; i++) 
+            return 1+countLongestPallindromic(ind1-1,ind2-1,first,second);
+        } 
+        //if not matching : then either reduce the either one index of 1st string or of another string 
+        else
         {
-            for (int j = 1; j <= ind2; j++) 
-            {
-                if (text1[i - 1] == text2[j - 1]) 
-                {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } 
-                else 
-                {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+            int take1=countLongestPallindromic(ind1-1,ind2,first,second); 
+            int take2=countLongestPallindromic(ind1,ind2-1,first,second); 
+            return max(take1,take2);
         }
-        
-        return dp[ind1][ind2];
     }
 };
