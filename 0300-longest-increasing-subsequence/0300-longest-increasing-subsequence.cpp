207022eb955839f0ac593641a20c3160dce4aaad
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-26 09:38:49 +0530

    Time: 258 ms (37.64%), Space: 296.5 MB (21.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -29,30 +29,56 @@
 // };
 
 
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums)  
+//     {  
+//         vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));  
+//         //we wana to store the ind and prev_index , but the intailly the prev_index is getting start from the -1 , so which not feasible for us , to access the -1
+//         return LIS(nums, 0, -1,dp);
+//     }
+
+//     int LIS(vector<int>& nums, int ind, int prev_index,vector<vector<int>>&dp) {
+//         if (ind == nums.size()) {
+//             return 0;
+//         }
+
+//         if(dp[ind][prev_index+1]!=-1)     ///Beacause if we use prev_index=-1 intiallly then that might be wrong , so prev_index+1
+//         {
+//             return dp[ind][prev_index+1];
+//         }
+//         int not_take = LIS(nums, ind + 1, prev_index,dp);
+//         int take = INT_MIN;
+//         if (prev_index == -1 || nums[ind] > nums[prev_index]) {
+//             take = 1 + LIS(nums, ind + 1, ind,dp);
+//         }
+
+//         return dp[ind][prev_index+1]=max(take, not_take);
+//     }
+// }; 
+
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums)  
-    {  
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));  
-        //we wana to store the ind and prev_index , but the intailly the prev_index is getting start from the -1 , so which not feasible for us , to access the -1
-        return LIS(nums, 0, -1,dp);
-    }
+    { 
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,0)); 
+        //as we cannot access the -1 index of the dp table so that's why we are using the nums.size()+1 
 
-    int LIS(vector<int>& nums, int ind, int prev_index,vector<vector<int>>&dp) {
-        if (ind == nums.size()) {
-            return 0;
-        }
-
-        if(dp[ind][prev_index+1]!=-1)     ///Beacause if we use prev_index=-1 intiallly then that might be wrong , so prev_index+1
+        for(int ind=nums.size()-1;ind>=0;ind--)
         {
-            return dp[ind][prev_index+1];
-        }
-        int not_take = LIS(nums, ind + 1, prev_index,dp);
-        int take = INT_MIN;
-        if (prev_index == -1 || nums[ind] > nums[prev_index]) {
-            take = 1 + LIS(nums, ind + 1, ind,dp);
-        }
-
-        return dp[ind][prev_index+1]=max(take, not_take);
+            for(int prev_index=ind-1;prev_index>=-1;prev_index--)
+            {
+                int not_take = dp[ind + 1][prev_index+1];
+                int take = INT_MIN;
+                if (prev_index == -1 || nums[ind] > nums[prev_index])  
+                {
+                    take = 1 + dp[ind+1][ind+1];
+                } 
+                dp[ind][prev_index+1]=max(take,not_take);
+        
+            }
+        } 
+        return dp[0][-1+1];
     }
 };
