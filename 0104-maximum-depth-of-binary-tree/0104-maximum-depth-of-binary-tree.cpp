cf42b817bc4630c38ffc7283792aaf2fed7ade2f
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-06-28 20:29:34 +0530

    Time: 0 ms (100%), Space: 19.2 MB (13.86%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -9,26 +9,65 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root)  
+//     {  
+//         if(root==NULL)
+//         {
+//             return 0;
+//         } 
+
+//         return find_max_depth(root);
+//     }  
+
+//     int find_max_depth(TreeNode* root)
+//     {
+//         if(root==NULL)
+//         {
+//             return 0;
+//         } 
+//         int lc=find_max_depth(root->left); 
+//         int rc=find_max_depth(root->right); 
+//         return 1+max(lc,rc);
+//     }
+
+// }; 
+
+  
+  class Solution {
 public:
     int maxDepth(TreeNode* root)  
     {  
+        //2.By using the level order traversal: 
+        queue<TreeNode*>que1; 
+        queue<TreeNode*>que2;   
         if(root==NULL)
         {
             return 0;
         }
-        return find_max_depth(root);
-    }  
-
-    int find_max_depth(TreeNode* root)
-    {
-        if(root==NULL)
+        que1.push(root); 
+        int count=0;  
+        do
         {
-            return 0;
-        } 
-        int lc=find_max_depth(root->left); 
-        int rc=find_max_depth(root->right); 
-        return 1+max(lc,rc);
-    }
+            while(!que1.empty())
+            {
+                TreeNode* T=que1.front(); 
+                que1.pop(); 
+                
+                if(T->left!=NULL)
+                {
+                    que2.push(T->left);
+                } 
 
-};
+                if(T->right!=NULL)
+                {
+                    que2.push(T->right);
+                } 
+            } 
+            swap(que1,que2);
+            count++;
+        }while(!que1.empty()); 
+        return count;
+    }
+  };
