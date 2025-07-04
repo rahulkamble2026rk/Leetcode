36124718f4221f7a5ce50b06681723631abc3aa9
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-07-04 18:45:47 +0530

    Time: 6 ms (68.46%), Space: 33.1 MB (23.59%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -4,53 +4,26 @@ public:
     priority_queue<int, vector<int>, greater<int>> que; 
     
     KthLargest(int k, vector<int>& nums)  
-    {
-            int n = nums.size();
-            ind = k;
+    {   
+        ind = k;
          
-         if(k<=nums.size())
-         {
-              for(int i=0;i<k;i++)
-                {
-                
-                    que.push(nums[i]);
-                        
+        for(int i=0;i<nums.size();i++)
+        {
             
-                }  
-                 for(int i=k;i<nums.size();i++)
-                {
-                    
-                    if(nums[i]>que.top())
-                    {
-                        que.pop();
-                        que.push(nums[i]);
-                    }
-                } 
-         }
-         else {
-             for(int i=0;i<n;i++)
-                {              
-                    que.push(nums[i]);     
-                }  
-         }
-     
-       
-         
+                que.push(nums[i]);
+
+        }      
     }
     
     int add(int val)  
     {  
-         cout<<ind<<" ";
-         if(que.size() < ind)
-         {
-            que.push(val);
-         }
-         else if(val>que.top())
-            {  
-                
-                que.pop();
-                que.push(val);
-            }
-         return que.top();
+        //  cout<<ind<<" ";
+         que.push(val);
+        while (que.size() > ind) {
+        que.pop();
+    }
+
+        return que.top();
+        
     }
 };
