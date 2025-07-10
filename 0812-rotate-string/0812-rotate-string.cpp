129f029a4127b39c902f0a850b5d91bccfb8248e
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-07-10 20:33:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.64%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,48 +1,19 @@
-// class Solution {
-// public:
-//     bool rotateString(string s, string goal) 
-//     { 
-//         if(s.size()!=goal.size())
-//         {
-//             return false;
-//         }
-//        s=s+s; 
-       
-//        string ans; 
-//        for(int i=0;i<s[i];i++)
-//        {
-//           if(s[i]!=32)
-//           {
-//              ans+=s[i];
-//           }
-//        }
-        
-//         int x=ans.find(goal); 
-//         if(x!=-1)
-//         {
-//             return true;
-//         } 
-//        return false;
-//     }
-// };
-
 class Solution {
 public:
-    bool rotateString(string s, string goal) 
-    { 
+    bool rotateString(string s, string goal)  
+    {
         
-        if(s.size() != goal.size()) {
+
+        if(s.size()!=goal.size())
+        {
             return false;
         }
+        s=s+s; 
+        if(s.find(goal)==-1)
+        {
+            return false;
+        }
+        return true;
 
-     
-        s = s + s;
-
-        
-        if(s.find(goal) != string::npos) {
-            return true;
-        } 
-
-        return false;
     }
 };
