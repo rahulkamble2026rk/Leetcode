35a4083770b253547680a9e5f50ff2d8882786b6
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-08-14 10:50:27 +0530

    Time: 80 ms (62.88%), Space: 73.4 MB (44.64%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,3 +1,64 @@
+// class Solution {
+// public: 
+
+//     // int modulo=109 + 7 
+//      const int MOD = 1000000007;
+//     int numberOfWays(int n, int x)  
+//     {
+//         //as there is n, and x(as the power that we have to take) 
+//         //step1: foramtion of the vector which containg the values as the square of the numbers upto the n: 
+//         vector<int>vct; 
+//         for(int i=1;i<=n;i++)
+//         {   
+//              long long val = pow(i, x); 
+//              if(val>n)
+//              {
+//                 break;
+//              }
+//             vct.push_back((int)val);
+//         }  
+
+//         // for(int i:vct)
+//         // {
+//         //     cout<<i<<" ";
+//         // }  
+//         vector<vector<int>>dp(vct.size()+1,vector<int>(n+1,-1) );
+//         return count_ways(vct,n,0,dp);
+//     } 
+
+//     int count_ways(vector<int>&vct,int n,int ind,vector<vector<int>>&dp)
+//     {   
+
+//         if(n==0)
+//         {   
+//             // cout<<"I am counted";
+//             return 1;
+//         }  
+       
+//         if(ind>=vct.size())
+//         {
+//             return 0;
+//         }  
+
+//          if(dp[ind][n]!=-1)
+//         {
+//             return dp[ind][n];
+//         }
+
+//         //take it:  
+//         int take=0;
+//         if(n>=vct[ind])
+//         {
+//             take=count_ways(vct,n-vct[ind],ind+1,dp);
+//         } 
+//         int not_take=count_ways(vct,n,ind+1,dp); 
+//         return dp[ind][n]=((take+not_take)%MOD);
+//     }
+// }; 
+
+
+
+//By using the tabulzation:
 class Solution {
 public: 
 
@@ -5,8 +66,7 @@ public:
      const int MOD = 1000000007;
     int numberOfWays(int n, int x)  
     {
-        //as there is n, and x(as the power that we have to take) 
-        //step1: foramtion of the vector which containg the values as the square of the numbers upto the n: 
+        
         vector<int>vct; 
         for(int i=1;i<=n;i++)
         {   
@@ -18,40 +78,30 @@ public:
             vct.push_back((int)val);
         }  
 
-        // for(int i:vct)
-        // {
-        //     cout<<i<<" ";
-        // }  
-        vector<vector<int>>dp(vct.size()+1,vector<int>(n+1,-1) );
-        return count_ways(vct,n,0,dp);
+       
+        vector<vector<int>>dp(vct.size()+1,vector<int>(n+1,0)); 
+        int m=vct.size();
+        //Base case: 1 way to make sum=0; 
+
+        dp[m][0]=1;
+        //Bottom up filling: 
+        for(int ind=m-1;ind>=0;ind--)
+        {
+            for(int sum=0;sum<=n;sum++)
+            {
+                int not_take=dp[ind+1][sum]; 
+
+                int take=0; 
+                if(sum>=vct[ind])
+                {
+                    take=dp[ind+1][sum-vct[ind]];
+                } 
+
+                dp[ind][sum]=(take+not_take)%MOD;
+            }
+        }
+        return dp[0][n];
     } 
 
-    int count_ways(vector<int>&vct,int n,int ind,vector<vector<int>>&dp)
-    {   
-
-        if(n==0)
-        {   
-            // cout<<"I am counted";
-            return 1;
-        }  
-       
-        if(ind>=vct.size())
-        {
-            return 0;
-        }  
-
-         if(dp[ind][n]!=-1)
-        {
-            return dp[ind][n];
-        }
-
-        //take it:  
-        int take=0;
-        if(n>=vct[ind])
-        {
-            take=count_ways(vct,n-vct[ind],ind+1,dp);
-        } 
-        int not_take=count_ways(vct,n,ind+1,dp); 
-        return dp[ind][n]=((take+not_take)%MOD);
-    }
+    
 };
