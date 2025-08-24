ee0b30de1cb6e81a1d790c50003972577193b046
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-08-24 11:49:06 +0530

    Time: 573 ms (0%), Space: 8.6 MB (100%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -6,7 +6,11 @@ public:
         int oddSum=0;  
         int flagEven=0;  
         int flagOdd=0;
-        int i=1;
+        int i=1; 
+        if(n==1) 
+        {
+            return 1;
+        }
        while(flagOdd<n && flagEven<=n)
         {
             if(i%2==0)
@@ -24,9 +28,20 @@ public:
         }  
 
 
+         
+         //gcd: i=min(evenSum,oddSum) , and then go upto the 1:  
+         i=min(evenSum,oddSum); 
+         int k=0;
+         for( k=i;k>=1;k--)
+         {
+            if(evenSum%k==0 && oddSum%k==0)
+            {
+                break;
+            }
+         }
 
-
-        cout<<"Odd:"<<oddSum<<" "<<"Even: "<<evenSum<<endl;
-        return __gcd(evenSum,oddSum);
+        // cout<<"Odd:"<<oddSum<<" "<<"Even: "<<evenSum<<endl;
+        // return __gcd(evenSum,oddSum); 
+        return k;
     }
 };
