411bd985a40bc93feedf7be6fbe052d8ac7c3e87
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-08-29 16:05:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (12.44%) - LeetHub

---
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m)  
+    {
+        //chooses one of the flower outoff present two layeres of the flower: 
+        //1st layer is of n : from 1 to n 
+        //2nd layer is of m: from 1 to m 
+
+         //at the end if thre are no flowers are left at all , then the current player captures their opponent and wins the game: 
+
+         //alice wants to win the game ,  
+
+        return (long long )n*m/2;
+    }
+};
