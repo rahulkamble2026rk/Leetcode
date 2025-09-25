0de935994faecb17d0cfedfaa2d3d051ad41e709
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2025-09-25 11:46:56 +0530

    Time: 0 ms (100%), Space: 13.4 MB (22.22%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,42 +1,75 @@
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) 
+//     {
+//         int m=triangle.size(); 
+//         int n=triangle[0].size();  
+//         vector<vector<int>>dp(m);  
+//         for(int i=0;i<m;i++)
+//         {
+//             dp[i]=vector<int>(triangle[i].size(),-1);
+//         } 
+      
+//         return minimumcount(0,0,m,triangle,dp);
+//     } 
+//     int minimumcount(int i,int j,int finalrow,vector<vector<int>>&triangle,vector<vector<int>>&dp)
+//     {
+//         if(i==finalrow-1)
+//         {
+//             return triangle[i][j];
+//         } 
+       
+//         if(j>=triangle[i].size())
+//         {
+//             return 0;
+//         }  
+
+//          if(dp[i][j]!=-1)
+//         {
+//             return dp[i][j];
+//         }  
+        
+
+//         int first=triangle[i][j]+minimumcount(i+1,j,finalrow,triangle,dp);  
+//         // cout<<"I am first"<<first<<endl;
+//         int second=triangle[i][j]+minimumcount(i+1,j+1,finalrow,triangle,dp);  
+//         // cout<<"I am second"<<second<<endl;
+//         int mini=min(first,second); 
+//         // cout<<"I am going to add"<<mini<<endl;
+//         return dp[i][j]=min(first,second);
+//     }
+// }; 
+
+
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle)  
+    int minimumTotal(vector<vector<int>>& triangle) 
     {
-        int n=triangle.size(); 
-        int m=triangle[0].size(); 
-         int sum=0;   
-         vector<vector<int>>dp(n); 
-        //  vector<vector<int>> dp(n);
-        for (int i = 0; i < n; i++) {
-            dp[i] = vector<int>(triangle[i].size(), -1);
-        }
-
-         if(triangle.size()==1)
-         {
-            return triangle[0][0]; 
-         }  
-         return countMinpath(triangle,sum,0,0,dp);
-         
-    } 
-    int countMinpath(vector<vector<int>>&Triangle,int& sum,int i, int j,vector<vector<int>>&dp)
-    {
-         
-         if (i >= Triangle.size() || j >= Triangle[i].size()) {
-    return 1e9; // Large value to avoid choosing this path
-} 
-
-        if(i==Triangle.size()-1)
+        int m=triangle.size(); 
+        int n=triangle[0].size();  
+        vector<vector<int>>dp(m);  
+        for(int i=0;i<m;i++)
         {
-            return Triangle[i][j];
-        }
-          
-        
-         if(dp[i][j]!=-1)
-         {
-            return dp[i][j];
-         }
-         int first=countMinpath(Triangle,sum,i+1,j,dp); 
-         int second=countMinpath(Triangle,sum,i+1,j+1,dp);
-         return dp[i][j]=Triangle[i][j]+min(first,second);
-    }
+            dp[i]=vector<int>(triangle[i].size(),-1);
+        } 
+      
+
+        for(int i=0;i<m;i++)
+        {
+            dp[m-1][i]=triangle[m-1][i];
+        } 
+
+
+        for(int i=m-2;i>=0;i--)    //as the memoization starts from  top->bottom but here tabulzation always work in the diff. manner so it will be in bottom->top 
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int first=triangle[i][j]+dp[i+1][j]; 
+                int second=triangle[i][j]+dp[i+1][j+1]; 
+                dp[i][j]=min(first,second);
+            }
+        } 
+        return dp[0][0];
+    } 
+   
 };
