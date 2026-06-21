55736df5752d6ddcf6070e9af46090233b301552
Rahul Sanjay Kamble <rahulkamble2026rk@gmail.com>
2026-06-21 18:54:47 +0530

    Time: 25 ms (12.19%), Space: 22.2 MB (28.85%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxDistance(string moves)  
+    {
+        //before placing the element at the _ check the left and right directions first,  
+        int x=0,y=0,ct=0;
+
+        for(int i=0;i<moves.size();i++)
+        {
+            char ch=moves[i]; 
+            if(ch=='L')
+            {
+                x+=-1;
+            } 
+            else if(ch=='R')
+            {  
+                x+=1;
+            } 
+            else if(ch=='D')
+            {
+                y-=1;
+            } 
+            else if(ch=='U')
+            {
+                y+=1;
+            } 
+            else 
+            {
+                ct++;
+            }
+        } 
+
+        int ans=(abs(x)+abs(y)); 
+        return ans+ct;
+    }
+};
